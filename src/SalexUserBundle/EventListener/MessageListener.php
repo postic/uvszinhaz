00838cd1731d5e6718ca8ee5515c0bfe7edf6c03
Stevan Postic <stevan.postic@cloudtech.company>
2017-09-29 13:33:36 +0200

    Navbar navigation

---
+++ src/SalexUserBundle/EventListener/MessageListener.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stevan
+ * Date: 29.9.17.
+ * Time: 09.07
+ */
+namespace SalexUserBundle\EventListener;
+// ...
+
+use Avanzu\AdminThemeBundle\Event\MessageListEvent;
+use FOS\UserBundle\Model\UserInterface;
+use SalexUserBundle\Entity\User;
+use SalexUserBundle\Model\MessageModel;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class MessageListener {
+
+    private $container;
+
+    public function __construct(ContainerInterface $container) {
+        $this->container = $container;
+    }
+
+    public function onListMessages(MessageListEvent $event) {
+
+        foreach($this->getMessages() as $message) {
+            $event->addMessage($message);
+        }
+
+    }
+
+    protected function getMessages() {
+
+        $userManager = $this->container->get('fos_user.user_manager');
+        $from = $userManager->findUserBy(array('id'=>6));
+
+        // retrieve your message models/entities here
+        $items = array(
+            new MessageModel($from, 'Created reservation', new \DateTime(), $from),
+            new MessageModel($userManager->findUserBy(array('id'=>50)), 'Created reservation', new \DateTime(), $from),
+            new MessageModel($userManager->findUserBy(array('id'=>51)), 'Created reservation', new \DateTime(), $from),
+        );
+        return $items;
+    }
+
+}
---
+++ src/SalexUserBundle/Model/MessageModel.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stevan
+ * Date: 29.9.17.
+ * Time: 09.56
+ */
+
+namespace SalexUserBundle\Model;
+use Avanzu\AdminThemeBundle\Model\MessageInterface as ThemeMessage;
+use SalexUserBundle\Entity\User;
+
+/**
+ * Simple implementation of the MessageInterface
+ *
+ */
+class MessageModel implements ThemeMessage
+{
+    /**
+     * Holds the sender
+     *
+     * @var UserInterface
+     */
+    protected $from;
+
+    /**
+     * holds the Recipient
+     *
+     * @var UserInterface
+     */
+    protected $to;
+
+    /**
+     * holds the date sent
+     *
+     * @var \DateTime
+     */
+    protected $sentAt;
+
+    /**
+     * holds the subject
+     *
+     * @var string
+     */
+    protected $subject;
+
+    /**
+     * Creates a new MessageModel object with the given values.
+     *
+     * SentAt will be set to the current DateTime when null is given.
+     *
+     * @param UserInterface $from
+     * @param string        $subject
+     * @param null          $sentAt
+     * @param UserInterface $to
+     */
+    public function __construct(User $from = null, $subject = '', $sentAt = null, User $to = null)
+    {
+        $this->to = $to;
+        $this->subject = $subject;
+        $this->sentAt = $sentAt ?: new \DateTime();
+        $this->from = $from;
+    }
+
+    /**
+     * Set the sender
+     *
+     * @param \Avanzu\AdminThemeBundle\Model\UserInterface $from
+     *
+     * @return $this
+     */
+    public function setFrom(UserInterface $from)
+    {
+        $this->from = $from;
+
+        return $this;
+    }
+
+    /**
+     * Get the Sender
+     *
+     * @return \Avanzu\AdminThemeBundle\Model\UserInterface
+     */
+    public function getFrom()
+    {
+        return $this->from;
+    }
+
+    /**
+     * Set the date sent
+     *
+     * @param \DateTime $sentAt
+     *
+     * @return $this
+     */
+    public function setSentAt(\DateTime $sentAt)
+    {
+        $this->sentAt = $sentAt;
+
+        return $this;
+    }
+
+    /**
+     * Get the date sent
+     *
+     * @return \DateTime
+     */
+    public function getSentAt()
+    {
+        return $this->sentAt;
+    }
+
+    /**
+     * Set the subject
+     *
+     * @param string $subject
+     *
+     * @return $this
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    /**
+     * Get the subject
+     *
+     * @return string
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * Set the recipient
+     *
+     * @param \Avanzu\AdminThemeBundle\Model\UserInterface $to
+     *
+     * @return $this
+     */
+    public function setTo(UserInterface $to)
+    {
+        $this->to = $to;
+
+        return $this;
+    }
+
+    /**
+     * Get the recipient
+     *
+     * @return \Avanzu\AdminThemeBundle\Model\UserInterface
+     */
+    public function getTo()
+    {
+        return $this->to;
+    }
+
+    /**
+     * Get the identifier
+     *
+     * @return string
+     */
+    public function getIdentifier() {
+        return $this->getSubject();
+    }
+}
