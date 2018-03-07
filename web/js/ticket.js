fd8d3a7338695a8faab2bedd129be0eabbb8c941
steva <postic.stevan@nsinfo.co.rs>
2018-03-07 14:53:16 +0100

    Ticket

--- web/js/ticket.js
+++ web/js/ticket.js
@@ -21,79 +21,74 @@ $(document).ready(function() {
             '_bbbbbbbbbbbbbbbbbb_'
         ];
 
-        var $cart = $('#selected-seats'),
-            $counter = $('#counter'),
-            $total = $('#total'),
-            sc = $('#ticket-map').seatCharts({
-                map: $map,
-                seats: {
-                    f: {
-                        classes : 'first-class' //your custom CSS class
-                    },
-                    b: {
-                        classes : 'balcony-class' //your custom CSS class
-                    }
-
+        var sc = $('#ticket-map').seatCharts({
+            map: $map,
+            seats: {
+                f: {
+                    classes : 'first-class' //your custom CSS class
                 },
-                naming : {
-                    top : false,
-                    left : true,
-                    right : true,
-                    getId  : function(character, row, column) {
-                        if(row != 11 ) {
-                            column = parseInt(column)-1;
-                            return row + '_' + column.toString();
-                        }
-                        else return row + '_' + column;
-
-                    },
-                    getLabel : function (character, row, column) {
-                        if(row != 11 ) {
-                            column = parseInt(column)-1;
-                            return column.toString();
-                        }
-                        else return column;
-                    }
-                },
-                legend : {
-                    node : $('#legend'),
-                    items : [
-                        [ 'f', 'available',   'Slobodna sedišta' ],
-                        [ 'f', 'unavailable', 'Zauzeta sedišta']
-                    ]
-                },
-                click: function () {
-                    if (this.status() == 'available') {
-                        //let's create a new <li> which we'll add to the cart items
-                        // $('<li>'+this.data().category+' Seat # '+this.settings.label+': <b>$'+this.data().price+'</b> <a href="#" class="cancel-cart-item">[cancel]</a></li>')
-
-                        create_row(this.settings.id);
-
-                        /*$('<li>'+this.data().category+': <b>'+this.settings.id+'</b></li>')
-                            .attr('id', 'cart-item-'+this.settings.id)
-                            .data('seatId', this.settings.id)
-                            .appendTo($cart);
-                        $counter.text(sc.find('selected').length+1);
-                        $total.text(recalculateTotal(sc)+this.data().price);*/
-                        return 'selected';
-                    } else if (this.status() == 'selected') {
-                        //update the counter
-                        $counter.text(sc.find('selected').length-1);
-                        //and total
-                        $total.text(recalculateTotal(sc)-this.data().price);
-                        //remove the item from our cart
-                        $('#cart-item-'+this.settings.id).remove();
-                        //seat has been vacated
-                        return 'available';
-                    } else if (this.status() == 'unavailable') {
-                        //seat has been already booked
-                        return 'unavailable';
-                    } else {
-                        return this.style();
-                    }
+                b: {
+                    classes : 'balcony-class' //your custom CSS class
                 }
-            });
+            },
+            naming : {
+                top : false,
+                left : true,
+                right : true,
+                getId  : function(character, row, column) {
+                    if(row != 11 ) {
+                        column = parseInt(column)-1;
+                        return row + '_' + column.toString();
+                    }
+                    else return row + '_' + column;
+                },
+                getLabel : function (character, row, column) {
+                    if(row != 11 ) {
+                        column = parseInt(column)-1;
+                        return column.toString();
+                    }
+                    else return column;
+                }
+            },
+            legend : {
+                node : $('#legend'),
+                items : [
+                    [ 'f', 'available',   'Slobodna sedišta' ],
+                    [ 'f', 'unavailable', 'Zauzeta sedišta']
+                ]
+            },
+            click: function () {
+                if (this.status() == 'available') {
+                    create_row_ticket(this.settings.id);
 
+                    // Izracunavanje sume
+                    var $sum = 0;
+                    $('.cart-item-price').each(function() {
+                        $sum = $sum + parseInt($(this).text());
+                    });
+                    $('#sum').text($sum);
+
+                    return 'selected';
+                } else if (this.status() == 'selected') {
+                    //remove the item from our cart
+                    $('#cart-item-'+this.settings.id).remove();
+
+                    // Izracunavanje sume
+                    var $sum = 0;
+                    $('.cart-item-price').each(function() {
+                        $sum = $sum + parseInt($(this).text());
+                    });
+                    $('#sum').text($sum);
+
+                    return 'available';
+                } else if (this.status() == 'unavailable') {
+                    //seat has been already booked
+                    return 'unavailable';
+                } else {
+                    return this.style();
+                }
+            }
+        });
         load_seats(sc);
     }
 
@@ -110,14 +105,6 @@ $(document).ready(function() {
     });
 
 });
-
-function recalculateTotal(sc) {
-    var total = 0;
-    sc.find('selected').each(function () {
-        total += this.data().price;
-    });
-    return total;
-}
 
 function get_seats(sc) {
     var retval = [];
@@ -143,10 +130,73 @@ function load_seats(sc) {
     })
 }
 
-function create_row(item){
+function create_row_ticket($data){
+
     var $cart = $('#tickets tbody');
-    $('<tr><td>'+item.replace("_", "/")+'</td><td><select class="form-control"><option>Besplatna</option><option>Pojedinačna</option></select></td><td></td></tr>')
-        .attr('id', 'cart-item-'+item)
-        .data('seatId', item)
-        .appendTo($cart);
+    // add row
+    $cart
+        .append($('<tr>')
+            .attr('id', 'cart-item-'+$data)
+            .data('seatId', $data)
+            .append($('<td>')
+                .append($data.replace("_", "/")))
+            .append($('<td>')
+                .append($('<select>')
+                    .attr('id', 'cart-item-type-'+$data)
+                    .addClass('form-control')))
+            .append($('<td>')
+                .append($('<span>')
+                    .attr('id', 'cart-item-price-'+$data)
+                    .addClass('cart-item-price')
+                    .append($performance.cena[1]))));
+
+    $.each($performance.cena, function(key, value) {
+
+        var $text;
+        switch(key) {
+            case '1':
+                $text = 'Pojedinačna';
+                break;
+            case '2':
+                $text = 'Grupna';
+                break;
+            case '3':
+                $text = 'Studentska';
+                break;
+            case '4':
+                $text = 'Penzionerska';
+                break;
+            case '5':
+                $text = 'Besplatna';
+                break;
+            case '6':
+                $text = 'Stručna';
+                break;
+        }
+
+        $('#cart-item-type-'+$data)
+            .append($('<option></option>')
+                .attr('value',key)
+                .attr('data-entity-id',value)
+                .text($text));
+    });
+    $('#cart-item-type-'+$data).val(1);
+
+    // Izracunavanje sume
+    $('#cart-item-type-'+$data).change(function() {
+        $sum = 0;
+        var $price = $('option:selected', this).attr('data-entity-id');
+        $('#cart-item-price-'+$data).text($price);
+        $('.cart-item-price').each(function() {
+            $sum = $sum + parseInt($(this).text());
+        });
+        $('#sum').text($sum);
+    });
+
+    // Izracunavanje sume
+    var $sum = 0;
+    $('.cart-item-price').each(function() {
+        $sum = $sum + parseInt($(this).text());
+    });
+    $('#sum').text($sum);
 }
